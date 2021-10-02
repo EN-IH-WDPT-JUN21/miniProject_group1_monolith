f9e7e6cde4f063751c8e8800c5bb3a49e6bfa550
Wojciech Drygas <wojtek.drygas@gmail.com>
2021-10-02 08:56:39 +0200

    fixed transactions

--- src/main/java/com/ironhack/midterm/bankingAPI/dao/accounts/Account.java
+++ src/main/java/com/ironhack/midterm/bankingAPI/dao/accounts/Account.java
@@ -17,7 +17,7 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @Table(name="accounts")
 @Inheritance(strategy = InheritanceType.JOINED)
-public abstract class Account {
+public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- src/main/java/com/ironhack/midterm/bankingAPI/repository/transactions/TransactionRepository.java
+++ src/main/java/com/ironhack/midterm/bankingAPI/repository/transactions/TransactionRepository.java
@@ -2,9 +2,11 @@ package com.ironhack.midterm.bankingAPI.repository.transactions;
 
 import com.ironhack.midterm.bankingAPI.dao.transactions.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction,Long> {
+    @Query ("SELECT t FROM Transaction t WHERE sender_account_id = :id ORDER BY transactionDate Desc")
     List<Transaction> findBySenderOrderByTransactionDateDesc(Long id);
 }
